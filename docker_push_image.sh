aaf63e0512965a2f8a8db28392ca659c9b4a1af7
Didoramadhan <setyajidapaa@gmail.com>
2024-09-11 13:24:19 +0700

    Added

--- docker_push_image.sh
+++ docker_push_image.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build -t item-app:v1 .
 docker images
-docker tag item-app:v1 ghcr.io/dapalearn/item-app:v1
-docker login ghcr.io
-docker push ghcr.io/dapalearn/item-app:v1
+docker tag item-app:v1 dapalearn1818/item-app:v1
+docker login 
+docker push dapalearn1818/item-app:v1
